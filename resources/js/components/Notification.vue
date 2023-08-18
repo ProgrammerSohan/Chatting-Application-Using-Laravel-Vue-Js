a5fa93c992060379137b6a09591325ae837a7148
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-19 05:19:22 +0600

    4.1_create notification event part1_a

--- resources/js/components/Notification.vue
+++ resources/js/components/Notification.vue
@@ -1,7 +1,18 @@
 <template>
-    notification
+   <div class="`alert ${data.class} my-2`">
+        {{ data.message }}
+
+   </div>
 </template>
 <script setup>
+    import { reactive } from 'vue';
+
+    const data = reactive({
+        message: 'Sohan has logged in',
+        class: 'alert-success',
+        show: false
+    });
+
 
 </script>
 <style>
